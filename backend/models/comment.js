fbdd4048e52bb21d590b31125390456ad16e671e
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2018-12-12 22:04:21 -0800

    Integrate database

---
+++ backend/models/comment.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CommentsSchema = new Schema(
+  {
+    author: String,
+    text: String
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Comment", CommentsSchema);
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const Comment = require("./models/comment");
 
 const app = express();
 const router = express.Router();
 
 const API_PORT = process.env.API_PORT || 3001;
+
+mongoose.connect('mongodb://localhost/test');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
