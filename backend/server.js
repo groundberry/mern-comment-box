db633e93390aea4172c4c811390534757ca32c09
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2018-12-16 16:48:52 -0800

    Get and post to the database

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 const API_PORT = process.env.API_PORT || 3001;
 
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
@@ -17,8 +17,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger("dev"));
 
-router.get("/", (req, res) => {
-  res.json({ message: "Hello, World!" });
+router.get('/comments', (req, res) => {
+  Comment.find((err, comments) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true, data: comments });
+  });
+});
+
+router.post('/comments', (req, res) => {
+  const comment = new Comment();
+  const { author, text } = req.body;
+  if (!author || !text) {
+    return res.json({
+      success: false,
+      error: 'You must provide an author and comment'
+    });
+  }
+
+  comment.author = author;
+  comment.text = text;
+  comment.save(err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
 });
 
 app.use("/api", router);
